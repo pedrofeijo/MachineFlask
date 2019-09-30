e7de94fe769bf7ea314bebce55a78f559325d7fa
Pedro Feijó <44382126+pedrofeijo@users.noreply.github.com>
2019-09-30 17:15:17 -0300

    Add files via upload

---
+++ Treinamento/run.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression
+
+data = pd.read_csv('/home/feijo/Documents/Treinamento/iris.csv', names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
+data.head()
+
+# class 1 = setosa
+
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+data['class'] = le.fit_transform(data['class'])
+data.head()
+
+X = np.array(data.drop(['class'], axis=1))
+y = np.array(data['class'])
+
+clf = LogisticRegression()
+clf.fit(X, y)
+
+clf = LogisticRegression()
+clf.fit(X, y)
+accuracy = clf.score(X, y)
+
+print(accuracy)
